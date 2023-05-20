2c4c5b5117e4445e4657a0afe32848e01962a5ff
Elmo <elmotabuzojr.ust@gmail.com>
2023-05-20 21:24:49 +0800

    updated nextjs config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
   publicRuntimeConfig: {
     APP_NAME: 'Keto Food Generator',
     API_DEVELOPMENT: 'http://127.0.0.1:5000/api',
-    API_PRODUCTION: 'https://food-generator-app-api.vercel.app/api',
+    API_PRODUCTION: 'https://ketofoodgenerator-api.herokuapp.com/api',
     DOMAIN_DEVELOPMENT: 'http://localhost:3000/',
     DOMAIN_PRODUCTION: 'https://ketofoodgenerator.com/',
     FB_APP_ID: '253159533892465',
-    PRODUCTION: false,
+    PRODUCTION: true,
   },
 };
 
