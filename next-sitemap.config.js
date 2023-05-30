feaef75caaad4943804a49c3fb5c5e24377eb2cc
Elmo <elmotabuzojr.ust@gmail.com>
2023-05-31 00:11:32 +0800

    added

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.ketofoodgenerator.com',
+  generateRobotsTxt: true, // (optional)
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `https://www.ketofoodgenerator.com/sitemap.xml`,
+      `https://www.ketofoodgenerator.com/server-sitemap.xml`,
+    ],
+  },
+};
