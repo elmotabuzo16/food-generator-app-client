6b77cb767421203b3ace1c8d0e0fbbc5bced4f6b
Elmo <elmotabuzojr.ust@gmail.com>
2023-05-31 17:31:33 +0800

    added favicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,8 +14,19 @@ export default function Document() {
           referrerPolicy='no-referrer'
         />
 
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicon.ico' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicon.ico' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-16x16.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-32x32.png'
+        />
+        <link rel='shortcut icon' href='/favicon-32x32.png' />
 
         <script
           async
